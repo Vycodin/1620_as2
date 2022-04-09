1f082650b7cf63d2d804f718c5b97ee04767fdb7
vycodin <kbyin2@gmail.com>
2022-04-09 06:38:30 -0700

    Add note button initial stages scripted

--- src/app.js
+++ src/app.js
@@ -5,3 +5,19 @@ const notes = [
     id: 1 
   }
 ]
+
+//create note taking buttoon
+
+const addButton = document.querySelector('i')
+const writingArea = document.querySelector('.write-note-area')
+let textarea = "<textarea id='note-textarea'></textarea><button id='save'>Save</button> <button id='cancel_note'>Cancel</button>"
+
+function addNote(evt){
+  writingArea.insertAdjacentHTML('afterbegin', textarea)
+  const cancel_btn = document.querySelector('cancel_note')
+  const save =  document.querySelector('save')
+  cancel_btn.addEventListener('click', remove_Note)
+  save.addEventListener('click', saveNote)
+}
+
+
