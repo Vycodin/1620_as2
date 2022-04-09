6020d08fdb6b0cfdaa2a1989c2cbc9914f178843
vycodin <kbyin2@gmail.com>
2022-04-09 14:40:42 -0700

    Save, cancel and text area added. Notes are properly pushed to array

--- src/app.js
+++ src/app.js
@@ -10,14 +10,34 @@ const notes = [
 
 const addButton = document.querySelector('i')
 const writingArea = document.querySelector('.write-note-area')
-let textarea = "<textarea id='note-textarea'></textarea><button id='save'>Save</button> <button id='cancel_note'>Cancel</button>"
+let textarea = "<textarea id='note-textarea'></textarea> <button id='save'>Save</button> <button id='cancel_note'>Cancel</button>"
 
 function addNote(evt){
   writingArea.insertAdjacentHTML('afterbegin', textarea)
-  const cancel_btn = document.querySelector('cancel_note')
-  const save =  document.querySelector('save')
+  const cancel_btn = document.querySelector('#cancel_note')
+  const save =  document.querySelector('#save')
   cancel_btn.addEventListener('click', remove_Note)
   save.addEventListener('click', saveNote)
 }
 
+addButton.addEventListener('click', addNote)
+
+
+
+//cancel button
+
+function remove_Note(evt){
+  while (writingArea.firstChild){
+    writingArea.removeChild(writingArea.firstChild)
+  }
+}
+
+//save button
+
+function saveNote(evt){
+  const textarea = document.querySelector('textarea')
+  arrayOfLines = textarea.value.split('\n')
+  notes.push({title: arrayOfLines[0], noteBody: textarea.value, id:notes.length +1})
+  console.log(notes)
+}
 
