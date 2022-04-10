11e579af21096d155757eb9c87b3ba97b7832308
vycodin <kbyin2@gmail.com>
2022-04-10 13:33:14 -0700

    functions cleaned up a little bit

--- src/app.js
+++ src/app.js
@@ -42,35 +42,29 @@ function saveNote(evt){
   note_title = arrayOfLines.shift()
   notes.push({title: note_title, noteBody: arrayOfLines, id:notes.length +1})
   console.log(notes)
+  save_side_nav(note_title, arrayOfLines)
+  remove_Note(evt)
+}
+function save_side_nav(title, body){
   const list_title = document.createElement('li')
   const list_p = document.createElement('p')
-  list_p.innerText = note_title
+  list_p.innerText = title
   list_title.appendChild(list_p)
   notes_list.appendChild(list_title)
   list_title.addEventListener('click', () =>{
-    read_note(list_title)
+    read_note(list_title,  body)
   })
-  remove_Note(evt)
 }
-
 //read note section
 
 const  reading_area = document.querySelector('.read-note-area')
 
-function read_note(evt){
+function read_note(evt, body){
   const note_div = document.createElement('div')
   const note_P = document.createElement('p')
   const close_btn = document.createElement('button')
-  read_title = evt.innerText
-  console.log(read_title)
-  for (const note of notes){
-    if (note.title == read_title){
-      note_content = note.noteBody
-      console.log('bruh')
-    }
-  }
   close_btn.innerHTML = 'X'
-  note_P.innerHTML = note_content
+  note_P.innerHTML = body
   reading_area.appendChild(note_div)
   note_div.appendChild(note_P)
   note_div.appendChild(close_btn)
